a15b535fa54e80d0964b3d38dfb07dd28dc80fc3
1Mirage <rasimsolmaz55@gmail.com>
2023-10-06 20:16:51 +0300

    1

---
+++ F1-CLASS/28.08.2023 Start/class-notes/JS01_intro/intro/index.js
@@ -0,0 +1,1 @@
+console.warn("dat diri dat dirilili")
---
+++ F1-CLASS/28.08.2023 Start/class-notes/JS01_intro/variables-datatypes/app.js
@@ -0,0 +1,18 @@
+console.log("salatasaray");
+
+
+
+const gs = "salatasaray"
+
+console.warn(gs);
+
+
+
+const sayi = 77
+
+console.error(sayi);
+
+
+const sg = "sacit"
+
+console.log(sg+" "+gs)
